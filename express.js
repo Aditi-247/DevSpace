f1f416dd957ecbc7add6e4e0b4da7fd69efebf64
Aditi-247 <71731371+Aditi-247@users.noreply.github.com>
2022-03-04 19:50:28 +0530

    node and express setup complete

---
+++ express.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+
+app.get('/',(req, res)=>{
+    res.send("Now Server start!!");
+})
+
+app.get('/about',(req, res)=>{
+    res.send("about the server!!");
+})
+
+app.post('/submit', (req, res)=>{
+    const data = req.data.data;
+    // save it the the database and
+    res.send("success saved to the db");
+})
+
+app.listen(8000, ()=>{
+    console.log("server listening on 8000");
+})
